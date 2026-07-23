f6ff4e1177b29e39310de7818c42fa54692bd34b
ta-mi-writer <takeda.mitsumasa.writer@gmail.com>
2026-07-23 07:32:35 +0000

    Initial commit of project files
    Added basic project structure, including marimo
    notebook integration,
    dependency management with pyproject.toml, and
    development environment
    configuration.

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from oreilly-soccer-analytics!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_nb.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.23.14"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    def main():
+        print("Hello from oreilly-soccer-analytics!")
+
+    def _main_():
+        main()
+
+    _main_()
+    return
+
+
+if __name__ == "__main__":
+    app.run()
